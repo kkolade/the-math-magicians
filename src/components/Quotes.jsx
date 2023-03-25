691b93c654adfc2edcc0fe40e42e0849e0a9113d
Kola Kolade <kolakolade@gmail.com>
2023-03-25 10:07:25 +0100

    Create a Quote component

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -4,12 +4,19 @@ const Quotes = () => {
   const [quote, setQuote] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const url = 'https://api.api-ninjas.com/vi/quotes';
 
   useEffect(() => {
     const getQuote = async () => {
       setIsLoading(true);
       try {
-        const fetchQuote = await fetch('https://api.api-ninjas.com/v1/quotes?');
+        const fetchQuote = await fetch(url, {
+          method: 'GET',
+          header: {
+            'Content-Type': 'application/json',
+            'X-Api-Key': 'tgGQLF0ayPX9ZzYdnazDEA==ZUlyDiE4HJM8YHuy',
+          },
+        });
         const fetchedQuote = await fetchQuote.json();
         setQuote(fetchedQuote);
       } catch (error) {
@@ -20,11 +27,28 @@ const Quotes = () => {
     getQuote();
   }, [setQuote, setIsLoading]);
 
-  if (hasError) return <div>Error getting the quotes!!!</div>;
-  if (isLoading) return <div>Loading the quotes...</div>;
+  if (hasError) {
+    return (
+      <div className="Quotes-container">
+        <span className="error">Error getting the quotes!!!</span>
+      </div>
+    );
+  }
+  if (isLoading) {
+    return (
+      <div className="Quotes-container">
+        <span>Loading the quotes...</span>
+      </div>
+    );
+  }
   return (
-    <div>
-      <p>{quote}</p>
+    <div className="Quotes-container">
+      <p>
+        &quot;
+        {quote[0].quote}
+        &quot;
+      </p>
+      <p>-{quote[0].author}</p>
     </div>
   );
 };

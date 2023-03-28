1a7a2446a1e8e4aa114df70ff1e06dbd578dd153
Kola Kolade <kolakolade@gmail.com>
2023-03-28 16:36:13 +0100

    Implement routing using React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './app.css';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
 import Quote from './components/Quote';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+      <nav>
+        <h1>
+          <Link to="/">Math Magicians</Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quotes</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 

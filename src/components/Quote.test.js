387ddbcd5e4679b9cba5630d16b3430e710d9470
cabraham1 <christopherabraham8@gmail.com>
2023-03-31 10:28:19 +0100

    jest snapshot test for Quote component

--- src/components/Quote.test.js
+++ src/components/Quote.test.js
@@ -1,34 +1,11 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Quote from './Quote';
 
-test('renders a quote', async () => {
-  const mockData = {
-    quote: 'To be or not to be, that is the question.',
-    author: 'William Shakespeare',
-  };
-  global.fetch = jest.fn().mockResolvedValue({
-    json: jest.fn().mockResolvedValue([mockData]),
-  });
-
-  const { getByText } = render(<Quote />);
-
-  expect(getByText('Loading a quote...')).toBeInTheDocument();
-
-  await waitFor(() => {
-    expect(getByText(`"${mockData.quote}"`)).toBeInTheDocument();
-    expect(getByText(mockData.author)).toBeInTheDocument();
-  });
-});
-
-test('renders an error message if the API call fails', async () => {
-  global.fetch = jest.fn().mockRejectedValue(new Error('Failed to fetch'));
-
-  const { getByText } = render(<Quote />);
-
-  expect(getByText('Loading a quote...')).toBeInTheDocument();
-
-  await waitFor(() => {
-    expect(getByText('Error getting quote!!!')).toBeInTheDocument();
+describe('Quote component', () => {
+  it('should render correctly', () => {
+    const component = renderer.create(<Quote />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
